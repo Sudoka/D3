1e1ac642f9c65507ead395487f85a22e0dc3368c
Srđan Rašić <srdan.rasic@gmail.com>
2012-08-31 20:29:16 +0200

    Initial Commit

---
+++ D3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  D3
+//
+//  Created by Srđan Rašić on 8/31/12.
+//  Copyright (c) 2012 Srđan Rašić. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
